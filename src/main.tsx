47fd0d54a5fdb748d71fa25fdf3dddcabeb14c1b
iceyisaak <me@iceyisaak.com>
2023-11-10 16:32:30 +0100

    Cleaned up code

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
+import { Provider } from 'jotai'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/app.tsx'
-import { Provider } from 'jotai'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './styles/global-style.scss'
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 
 const queryClient = new QueryClient()
@@ -14,7 +13,6 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
       <Provider>
         <App />
       </Provider>
