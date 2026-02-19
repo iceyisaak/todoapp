70a40c999a48acfc44b76b691f9fb5dcb2f7496d
iceyisaak <me@iceyisaak.com>
2026-02-19 15:47:53 +0100

    Resolved Type errors

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,41 +1,37 @@
-import { useTodoContext } from '../../contexts/todo-context'
-import TaskForm from '../task-form/task-form'
-import TaskList from '../task-list/task-list'
+import { useTodoContext } from "../../contexts/todo-context";
+import TaskForm from "../task-form/task-form";
+import TaskList from "../task-list/task-list";
 
-import { MdOutlineClose } from 'react-icons/md'
+import { MdOutlineClose } from "react-icons/md";
 
-import style from './app.module.scss'
+import style from "./app.module.scss";
 
 const App = () => {
-
-  const { deleteAllTasks, tasks } = useTodoContext()
+  const { deleteAllTasks, tasks } = useTodoContext();
 
   return (
-    <>
-      <div className={`${style['container']}`}>
-        <main className={`${style['main']}`}>
-          {
-            tasks.length > 0 &&
-            <MdOutlineClose
-              title='Delete All Tasks'
-              onClick={deleteAllTasks}
-              className={`
-                ${'pointer'}
-                ${style['btn-delall']}
-                ${style['btn-delete-all']}
+    <div className={`${style["container"]}`}>
+      <main className={`${style["main"]}`}>
+        {tasks.length > 0 && (
+          <MdOutlineClose
+            title="Delete All Tasks"
+            onClick={deleteAllTasks}
+            className={`
+                ${"pointer"}
+                ${style["btn-delall"]}
+                ${style["btn-delete-all"]}
               `}
-            />
-          }
-          <header className={`${style['header']}`}>
-            <h1 className={`${style['h1']}`}>TodoApp</h1>
-            <h4 className={`${style['h4']}`}>React Context API</h4>
-          </header>
-          <TaskForm />
-          <TaskList />
-        </main>
-      </div>
-    </>
-  )
-}
+          />
+        )}
+        <header className={`${style["header"]}`}>
+          <h1 className={`${style["h1"]}`}>TodoApp</h1>
+          <h4 className={`${style["h4"]}`}>React Context API</h4>
+        </header>
+        <TaskForm />
+        <TaskList />
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/contexts/todo-context.tsx
+++ src/contexts/todo-context.tsx
@@ -1,134 +1,113 @@
-import { ReactNode, createContext, useContext, useEffect, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { uuidv7 } from "uuidv7";
 
-import { type Task, type TodoContext } from '../types/todo'
+import { type Task, type TodoContext } from "../types/todo";
 
-
-const TodoContext = createContext<TodoContext>(null!)
+const TodoContext = createContext<TodoContext>(null!);
 
 const TodoContextProvider = ({ children }: { children: ReactNode }) => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEditing, setIsEditing] = useState<Task | null>(null);
 
-    const [tasks, setTasks] = useState<Task[]>([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [isEditing, setIsEditing] = useState<Task | null>(null)
+  useEffect(() => {
+    fetchSavedTasks();
+  }, []);
 
-
-    useEffect(() => {
-        fetchSavedTasks()
-    }, [])
-
-
-    const fetchSavedTasks = () => {
-        setIsLoading(true)
-        const savedTasks = localStorage.getItem("todo-list")
-        if (savedTasks) {
-            setTasks(JSON.parse(savedTasks))
-        }
-        setIsLoading(false)
+  const fetchSavedTasks = () => {
+    setIsLoading(true);
+    const savedTasks = localStorage.getItem("todo-list");
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
     }
+    setIsLoading(false);
+  };
 
+  const addTask = (taskTitle: string) => {
+    const addedTask = {
+      taskId: uuidv7(),
+      taskTitle,
+      isCompleted: false,
+    };
+    const tasksArray = [...tasks, addedTask];
+    setTasks(tasksArray);
+    localStorage.setItem("todo-list", JSON.stringify(tasksArray));
+  };
 
-
-    const addTask = (taskTitle: string) => {
-        const addedTask = {
-            taskId: uuidv4(),
-            taskTitle,
-            isCompleted: false
-        }
-        const tasksArray = [...tasks, addedTask]
-        setTasks(tasksArray)
-        localStorage.setItem("todo-list", JSON.stringify(tasksArray))
+  const deleteTask = (taskId: string) => {
+    const deleteTaskConfirm = window.confirm("Delete this task?");
+    if (deleteTaskConfirm) {
+      const filterOutDeletedTask = tasks.filter((task) => {
+        return task.taskId !== taskId;
+      });
+      setTasks(filterOutDeletedTask);
+      localStorage.setItem("todo-list", JSON.stringify(filterOutDeletedTask));
     }
+  };
 
-
-
-    const deleteTask = (taskId: string) => {
-        const deleteTaskConfirm = window.confirm('Delete this task?')
-        if (deleteTaskConfirm) {
-            const filterOutDeletedTask = tasks.filter(
-                (task) => {
-                    return task.taskId !== taskId
-                }
-            )
-            setTasks(filterOutDeletedTask)
-            localStorage.setItem("todo-list", JSON.stringify(filterOutDeletedTask))
-        }
+  const deleteAllTasks = () => {
+    const deleteAllTasksConfirm = window.confirm("Delete All Tasks?");
+    if (deleteAllTasksConfirm) {
+      localStorage.removeItem("todo-list");
+      setTasks([]);
     }
+  };
 
+  const selectTaskToEdit = (task: Task) => {
+    const selectedTask = {
+      taskId: task.taskId,
+      taskTitle: task.taskTitle,
+      isCompleted: task.isCompleted,
+    };
+    setIsEditing(selectedTask);
+  };
 
+  const editTask = (taskId: string, taskTitle: string) => {
+    const editedTask = tasks.map((task) =>
+      task.taskId === taskId ? { ...task, taskTitle } : task,
+    );
+    setTasks(editedTask);
+    localStorage.setItem("todo-list", JSON.stringify(editedTask));
+    setIsEditing(null);
+  };
 
-    const deleteAllTasks = () => {
-        const deleteAllTasksConfirm = window.confirm('Delete All Tasks?')
-        if (deleteAllTasksConfirm) {
-            localStorage.removeItem("todo-list")
-            setTasks([])
-        }
-    }
+  const toggleTaskAsCompleted = (taskId: string) => {
+    const toggledTask = tasks.map((task) =>
+      task.taskId === taskId
+        ? { ...task, isCompleted: !task.isCompleted }
+        : task,
+    );
+    setTasks(toggledTask);
+    localStorage.setItem("todo-list", JSON.stringify(toggledTask));
+  };
 
+  const TodoContextValue = {
+    tasks,
+    addTask,
+    editTask,
+    deleteTask,
+    deleteAllTasks,
+    isLoading,
+    isEditing,
+    selectTaskToEdit,
+    toggleTaskAsCompleted,
+  };
 
-
-    const selectTaskToEdit = (task: Task) => {
-        const selectedTask = {
-            taskId: task.taskId,
-            taskTitle: task.taskTitle,
-            isCompleted: task.isCompleted
-        }
-        setIsEditing(selectedTask)
-    }
-
-
-    const editTask = (taskId: string, taskTitle: string) => {
-        const editedTask = tasks.map(
-            (task) => (
-                task.taskId === taskId ?
-                    { ...task, taskTitle } :
-                    task
-            )
-        )
-        setTasks(editedTask)
-        localStorage.setItem("todo-list", JSON.stringify(editedTask))
-        setIsEditing(null)
-    }
-
-
-
-    const toggleTaskAsCompleted = (taskId: string) => {
-        const toggledTask = tasks.map(
-            (task) => (
-                task.taskId === taskId ?
-                    { ...task, isCompleted: !task.isCompleted } :
-                    task
-            )
-        )
-        setTasks(toggledTask)
-        localStorage.setItem("todo-list", JSON.stringify(toggledTask))
-    }
-
-
-
-    const TodoContextValue = {
-        tasks,
-        addTask,
-        editTask,
-        deleteTask,
-        deleteAllTasks,
-        isLoading,
-        isEditing,
-        selectTaskToEdit,
-        toggleTaskAsCompleted
-    }
-
-
-    return (
-        <TodoContext.Provider value={TodoContextValue}>
-            {children}
-        </TodoContext.Provider >
-    )
-
-}
+  return (
+    <TodoContext.Provider value={TodoContextValue}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
 
 export const useTodoContext = () => {
-    return useContext(TodoContext)
-}
+  return useContext(TodoContext);
+};
 
-export { TodoContext, TodoContextProvider }
+export { TodoContext, TodoContextProvider };
