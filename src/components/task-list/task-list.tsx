b69a9c2575add5c5cbd90f96aae51391de50dd7e
iceyisaak <me@iceyisaak.com>
2026-02-19 15:56:20 +0100

    Cleaned up code

--- src/components/task-list/task-list.tsx
+++ src/components/task-list/task-list.tsx
@@ -1,30 +1,25 @@
-import { useTodoContext } from '../../contexts/todo-context'
-import TaskItem from "./task-item/task-item"
+import { useTodoContext } from "../../contexts/todo-context";
+import TaskItem from "./task-item/task-item";
 
-import style from './task-list.module.scss'
-
+import style from "./task-list.module.scss";
 
 const TaskList = () => {
+  const { tasks, isLoading } = useTodoContext();
 
-    const { tasks, isLoading } = useTodoContext()
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <div className={`${style["TaskList"]}`}>
+      {tasks.length < 1 && (
+        <p className={`${style["no-task"]}`}>+++ Task List is Empty +++</p>
+      )}
+      <ul>
+        {tasks.map((task) => (
+          <TaskItem key={task.taskId} data={task} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    return isLoading ? (
-        <p>Loading...</p>
-    ) : (
-        <div className={`${style['TaskList']}`}>
-            {
-                !isLoading && tasks.length < 1 &&
-                <p className={`${style['no-task']}`}>
-                    +++ Task List is Empty +++
-                </p>
-            }
-            <ul>
-                {tasks.map((task) => (
-                    <TaskItem key={task.taskId} data={task} />
-                ))}
-            </ul>
-        </div>
-    )
-}
-
-export default TaskList
+export default TaskList;
