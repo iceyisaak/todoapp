f0fa6bfc9e0b6f5248ab49f85722d516b3e71fab
iceyisaak <me@iceyisaak.com>
2026-02-19 16:04:22 +0100

    Cleaned up code

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/app/app.tsx'
-import { TodoContextProvider } from './contexts/todo-context'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/app";
+import { TodoContextProvider } from "./contexts/todo-context";
 
+import "./styles/global-style.scss";
 
-import './styles/global-style.scss'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TodoContextProvider>
       <App />
     </TodoContextProvider>
   </React.StrictMode>,
-)
+);
