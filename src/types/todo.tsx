920a5618b2e460fa9a9966cbc49b40c9fc7d9792
iceyisaak <me@iceyisaak.com>
2026-02-19 16:02:27 +0100

    Fixed typo

--- src/types/todo.tsx
+++ src/types/todo.tsx
@@ -1,19 +1,17 @@
-
 export type Task = {
-    taskId: string,
-    taskTitle: string,
-    isCompleted: boolean
-}
-
+  taskId: string;
+  taskTitle: string;
+  isCompleted: boolean;
+};
 
 export type TodoContext = {
-    tasks: Task[],
-    addTask: (taskTitle: string) => void,
-    deleteTask: (taskId: string) => void,
-    selectTaskToEdit: (task: Task) => void,
-    editTask: (taskId: string, tastTitle: string) => void,
-    isEditing: null | Task,
-    deleteAllTasks: () => void,
-    toggleTaskAsCompleted: (taskId: string) => void,
-    isLoading: boolean
-}
+  tasks: Task[];
+  addTask: (taskTitle: string) => void;
+  deleteTask: (taskId: string) => void;
+  selectTaskToEdit: (task: Task) => void;
+  editTask: (taskId: string, taskTitle: string) => void;
+  isEditing: null | Task;
+  deleteAllTasks: () => void;
+  toggleTaskAsCompleted: (taskId: string) => void;
+  isLoading: boolean;
+};
