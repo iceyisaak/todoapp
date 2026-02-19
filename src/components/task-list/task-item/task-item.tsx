230ed035b3d4b48e9b9b3ef364d228d589596249
iceyisaak <me@iceyisaak.com>
2026-02-19 15:52:55 +0100

    Cleaned up code

--- src/components/task-list/task-item/task-item.tsx
+++ src/components/task-list/task-item/task-item.tsx
@@ -1,46 +1,52 @@
-import { MdOutlineDelete, MdOutlineEdit } from 'react-icons/md'
-import { useTodoContext } from '../../../contexts/todo-context'
+import { MdOutlineDelete, MdOutlineEdit } from "react-icons/md";
+import { useTodoContext } from "../../../contexts/todo-context";
 
-import { type Task } from '../../../types'
-import style from './task-item.module.scss'
+import { type Task } from "../../../types";
+import style from "./task-item.module.scss";
 
 type TaskItem = {
-    data: Task
-}
+  data: Task;
+};
 
 const TaskItem = ({ data }: TaskItem) => {
+  const { deleteTask, selectTaskToEdit, toggleTaskAsCompleted } =
+    useTodoContext();
 
-    const { deleteTask, selectTaskToEdit, toggleTaskAsCompleted } = useTodoContext()
+  const onDeleteTask = () => {
+    deleteTask(data.taskId);
+  };
 
-    const onDeleteTask = () => {
-        deleteTask(data.taskId)
-    }
+  const onSelectEditTask = () => {
+    selectTaskToEdit(data);
+  };
 
-    const onSelectEditTask = () => {
-        selectTaskToEdit(data)
-    }
+  const onToggleTaskAsComplete = () => {
+    toggleTaskAsCompleted(data.taskId);
+  };
 
-    const onToggleTaskAsComplete = () => {
-        toggleTaskAsCompleted(data.taskId)
-    }
+  return (
+    <li className={`${style["TaskItem"]}`}>
+      <input
+        type="checkbox"
+        onChange={onToggleTaskAsComplete}
+        checked={data.isCompleted}
+        className={`${style["checkbox"]}`}
+      />
+      <span
+        className={`${style["task-name"]} ${data.isCompleted ? `${style["isCompleted"]}` : ``}`}
+      >
+        {data.taskTitle}
+      </span>
+      <MdOutlineEdit
+        onClick={onSelectEditTask}
+        className={`${"pointer"} ${style["item-btn"]}`}
+      />
+      <MdOutlineDelete
+        onClick={onDeleteTask}
+        className={`${"pointer"} ${style["item-btn"]}`}
+      />
+    </li>
+  );
+};
 
-    return (
-        <li className={`${style['TaskItem']}`}>
-            <input
-                type="checkbox"
-                onChange={onToggleTaskAsComplete}
-                checked={data.isCompleted ? true : false}
-                className={`${style['checkbox']}`}
-            />
-            <span className={`${style['task-name']} ${data.isCompleted ? `${style['isCompleted']}` : ``}`}>
-                {data.taskTitle}
-            </span>
-            <MdOutlineEdit onClick={onSelectEditTask} className={`${'pointer'} ${style['item-btn']}`} />
-            <MdOutlineDelete onClick={onDeleteTask} className={`${'pointer'} ${style['item-btn']}`} />
-        </li>
-    )
-}
-
-
-
-export default TaskItem
+export default TaskItem;
