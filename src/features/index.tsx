3cfde01891f5d7dda5868bdd8f4a83ec3c1f06bd
iceyisaak <me@iceyisaak.com>
2026-02-19 19:41:43 +0100

    Cleaned up repo

--- src/features/index.tsx
+++
@@ -1,2 +0,0 @@
-// export { initialState } from './initialState'
-// export { todoReducer } from './todo-reducer'
--- src/features/todo-feature/todo-initialstate.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "jotai"
-import { atomWithStorage, splitAtom } from "jotai/utils"
-import { Task } from "../../types"
-
-export const tasksAtom = atomWithStorage<Task[]>('task-list', [])
-export const tasksSplitAtom = splitAtom(tasksAtom)
-export const newTaskAtom = atom('')
-export const isLoadingAtom = atom(false)
-export const isEditingAtom = atom<null | Task>(null)
