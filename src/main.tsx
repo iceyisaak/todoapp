87867a5db394e1430c33b2c5dbd686789f9bbdad
iceyisaak <me@iceyisaak.com>
2026-02-20 09:28:34 +0100

    Cleaned up code

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,15 @@
-import { Provider } from "jotai";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app";
 
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./styles/global-style.scss";
 
-// const queryClient = new QueryClient();
 import { TodoContextProvider } from "./reducers/stores/todoStore";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TodoContextProvider>
-      <Provider>
-        <App />
-      </Provider>
+      <App />
     </TodoContextProvider>
   </React.StrictMode>,
 );
